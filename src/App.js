db7ce6c960aa08d5e5d9d48de35c36c869b2409b
Juancarlos Ramirez <jcramz1992@gmail.com>
2023-08-03 14:20:00 -0500

    test commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const App = () => {
-  return <div className='overflow-hidden'>react app</div>;
+  return <div className="overflow-hidden">reacto app</div>;
 };
 
 export default App;
