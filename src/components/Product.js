85f8d5662bc4f86e70836295972137c83bb0116b
Juancarlos Ramirez <jcramz1992@gmail.com>
2023-08-03 19:33:58 -0500

    Added category, price, and title text

--- src/components/Product.js
+++ src/components/Product.js
@@ -47,6 +47,14 @@ const Product = ({ product }) => {
           </Link>
         </div>
       </div>
+      {/*category, price & title*/}
+      <div>
+        <div className="text-sm capitalize text-gray-500 mb-1">{category}</div>
+        <Link to={`product/${id}`}>
+          <h2 className="font-semibold mb-1">{title}</h2>
+        </Link>
+        <h2 className="font-semibold">${price}</h2>
+      </div>
     </div>
   );
 };
