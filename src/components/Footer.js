23fdd8518f1804626a04e30a21dab8fbb977e162
Juancarlos Ramirez <jcramz1992@gmail.com>
2023-08-04 05:33:49 -0500

    added product description page and fixed routes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-primary py-12">
-      <div clasName="container mx-auto">
+      <div className="container mx-auto">
         <p className="text-white text-center">
           Copyright &copy; Ecommerce Shop 2023. All rights reserved.
         </p>
